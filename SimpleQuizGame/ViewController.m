0323f400a96955d3c6d17dc09b2d65754f939140
mjp2220 <mjp2220@columbia.edu>
2014-11-10 20:50:24 -0500

    Adding the functionality to load questions

--- SimpleQuizGame/ViewController.m
+++ SimpleQuizGame/ViewController.m
@@ -7,13 +7,18 @@
 //
 
 #import "ViewController.h"
+#import "QuizQuestion.h"
 
 @interface ViewController ()
+
 @property (strong, nonatomic) IBOutlet UILabel *questionLabel;
 @property (strong, nonatomic) IBOutlet UITextField *answerField;
 @property (strong, nonatomic) IBOutlet UILabel *answerLabel;
 - (IBAction)answerButton:(id)sender;
 - (IBAction)nextQuestionButton:(id)sender;
+
+@property (strong, nonatomic) NSMutableArray *questionsArray;
+@property (nonatomic) NSInteger currentIndex;
 
 @end
 
@@ -22,6 +27,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.questionsArray = [self loadQuestionsArray];
+    self.currentIndex = 0;
+    QuizQuestion *currentQuestion = [self currentQuestion];
+    [self.questionLabel setText:currentQuestion.question];
+    [self.answerLabel setHidden:TRUE];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +43,18 @@
 }
 
 - (IBAction)nextQuestionButton:(id)sender {
+}
+
+- (NSMutableArray*) loadQuestionsArray {
+    // Good to use this to keep it GENERIC. Could later load from a database, remote server, etc.
+    QuizQuestion *question1 = [[QuizQuestion alloc] initWithQuestion:@"What is the capital of Oregon?" andAnswer:@"Salem"];
+    QuizQuestion *question2 = [[QuizQuestion alloc] initWithQuestion:@"How many species of penguin are there?" andAnswer:@"17"];
+    QuizQuestion *question3 = [[QuizQuestion alloc] initWithQuestion:@"Who performs the song 'Hey Jude'?" andAnswer:@"The Beatles"];
+    return [[NSMutableArray alloc] initWithObjects:question1, question2, question3, nil];
+}
+
+- (QuizQuestion*) currentQuestion {
+    NSUInteger currentQuestionIndex = (NSUInteger) (self.currentIndex % [self.questionsArray count]);
+    return [self.questionsArray objectAtIndex:currentQuestionIndex];
 }
 @end
